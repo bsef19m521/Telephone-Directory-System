c815ebed3c8b41c838471e15dafd8eaaf8f1c7d3
bsef19m521 <bsef19m521@pucit.edu.pk>
2022-12-28 22:22:59 +0500

    UPdated Telephone.py File

--- Telephone.py
+++ Telephone.py
@@ -13,43 +13,120 @@ class Telephone_directory:
 			9: Enter to search by City : \n 
 			10: Enter more records: \n"""))
             if self.user_choice==1:
-                user_record(self)
+                add_record(self)
+
+            if self.user_choice==2:
+                view_record(self)
+
             if self.user_choice == 3:
                 update_record(self)
-        def user_record(self):
+
+            if self.user_choice==4:
+                delete_record(self)
+
+            if self.user_choice==5:
+                block_record(self)
+
+            if self.user_choice ==6:
+                unblock_record(self)
+
+            if self.user_choice==7:
+                searchRecord_Alpha(self)
+
+            if self.user_choice==8:
+                searchRecord_blood(self)
+
+            if self.user_choice==9:
+                searchRecord_city(self)
+
+        def add_record(self):
             while True:
-                user_input = int(input("""
+                self.user_input = int(input("""
                 1. Enter Your Name : \n
                 2. Enter your age : \n 
                 3. Enter your phone number : \n 
                 4. Enter your City Name: \n
                 5. Enter your Blood Group :\n 
                 6. Enter to exit your program.\n"""))
-                if user_input == 6 :
+                if self.user_input == 6 :
                     break
-        def update_record(self):
+
+        def view_record(self):
             while True:
-                user_input = int(input("""
-                1. Enter Your New Name : \n
-                2. Enter your New age : \n 
-                3. Enter your New phone number : \n 
-                4. Enter your New City Name: \n
-                5. Enter your Blood Group :\n 
-                6. Enter to exit your program.\n"""))
-                if user_input == 6 :
-                    break
+                self.user_input = int(input("Please enter user's phone number to view Record : "))
+                self.number = 35637434
+                self.name = "Ehtisham"
+                self.age = 21
+                self.blood_group = 'O+'
+                self.city = 'Okara'
+                print(f"""User with {self.number} has following details/records : 
+                          User's Name : {self.name}
+                          User's Age : {self.age}
+                          User's Blood Group : {self.blood_group} 
+                          User's City : {self.city} """)
+
+        def update_record(self):
+            self.list1 = [ ] 
+            self.user_input = int(input("Please Enter user Phone Number: "))
+            if self.user_input in self.list1:
+                i = 1
+                while i<2:
+                    self.name = input("Enter your name : ")
+                    self.age = input("Enter your age : ")
+                    i = i+1
+            else:
+                print(f"User with {self.user_input} does not exit!")
             
                 
+        def delete_record(self):
+            self.list1 = [ ] 
+            self.user_input = int(input("Please Enter user Phone Number: "))
+            if self.user_input in self.list1:
+                print(f"User with {self.user_input} deleted successfully!")
+            else:
+                print(f"User with {self.user_input} does not exit!")                
                 
+        
+        def block_record(self):
+            self.list1 = [ ] 
+            self.user_input = int(input("Please Enter user Phone Number: "))
+            if self.user_input in self.list1:
+                print(f"User with {self.user_input} blocked successfully!")
+            else:
+                print(f"User with {self.user_input} does not exit!")  
+
                 
+        def unblock_record(self):
+            self.blocked = [ ] 
+            self.user_input = int(input("Please Enter user Phone Number: "))
+            if self.user_input in self.blocked:
+                print(f"User with {self.user_input} unblocked successfully!")
+            else:
+                print(f"User with {self.user_input} does not exit in blocked list!")  
+
+        def searchRecord_Alpha(self):
+            self.list1 = ['Ehtisham','Ali','Ayesha','Dua','Adeen']
+            self.user_input = input("Please a Alphabet: ")
+            for i in self.list1:
+                if i.startswith(self.user_input):
+                    print(i)
+
+        
+        def searchRecord_city(self):
+            self.city = ['Ehtisham','Ali','Ayesha','Dua','Adeen']
+            self.user_input = input("Please a Alphabet: ")
+            for i in self.city:
+                if i == self.user_input:
+                    print(i)
                 
-                
-                
-                
-                
-                
-                
-                
+
+        def searchRecord_blood(self):
+            self.blood = ['Ehtisham','Ali','Ayesha','Dua','Adeen']
+            self.user_input = input("Please a Alphabet: ")
+            for i in self.blood:
+                if i == self.user_input:
+                    print(i)
+
         while True:
                 self.main_choice = int(input("""
                 1.Enter to login as a User : \n
